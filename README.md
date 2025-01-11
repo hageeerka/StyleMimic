7f8aa7fe3f6920d14a664ed2327b4d0bc4cd52ac
test_pp_3sem <go130913@gmail.com>
2025-01-11 21:46:18 +0500

    version 1_0

---
+++ README.md
@@ -0,0 +1,32 @@
+<p align="center">
+  <img src="https://github.com/kirillzhosul/ollama-telegram-ai/assets/79853674/c42b7bbe-9a9f-4220-8ec8-b150d5ce9c46" />
+</p>
+
+# Ollama Telegram Bot
+Contains Telegram bot that interacts with the Ollama API LLM models to answer user messages
+
+### Features
+- Translations for base languages (to endpoint `en` language, can be disabled)
+- Chat system with clearing and etc
+- Regenerating completion (dislike button)
+- WIP: Change model
+
+### Helping videos
+- Russian: https://www.youtube.com/watch?v=CS4zcNKnczI&t=151s
+
+### How to run
+- Install [Ollama](https://ollama.com/)
+- Edit config (`./bot/settings.py`)
+- Install requirements (`poetry`)
+- run `main.py`
+- [Optional] [Search for models](https://ollama.com/search)
+
+### TODO:
+- Images context
+
+### Special thanks
+- [@RuslanPeresy](https://github.com/RuslanPeresy) for promoting this project
+
+### Tests
+
+Tested with llama2 / some other ollama LLMs
---
+++ bot/settings.py
@@ -0,0 +1,31 @@
+"""
+Bot configuration.
+
+By default, will obtain required fields from environment variables,
+but you can override that with modifying defaults.
+
+RU: Используйте переменные окружения или поменяйте default полей
+"""
+
+import os
+
+# Telegram token obtained from Bot Father
+TELEGRAM_TOKEN = "7648104021:AAHM5l-EY-lVdTzlPZBbAJo0_LhuIctUwPE"
+
+# First message that will be sent to model as it was user.
+# If left blank or None will do not perform this operation.
+START_USER_MESSAGE = ""
+
+# Model system message.
+SYSTEM_MESSAGE = """You are a bot"""
+
+# Translator module, change from default `en` to something like `ru` to perform translation on user messages.
+# NOTICE: WILL USE TRANSLATOR API (NOT MODEL ONE)
+BASE_LANGUAGE = "en"
+
+# Ollama server configuration
+OLLAMA_API_HOST = os.getenv("OLLAMA_API_HOST", default="http://localhost:11434")
+OLLAMA_MODEL = os.getenv("OLLAMA_BOT_MODEL", default="qwen2.5-coder")
+OLLAMA_MODEL_TEMPERATURE = 1
+OLLAMA_KEEP_ALIVE = "5m"
+# TODO: OLLAMA_INSTALL_MISSING_MODELS
